e910e6794bf2b661e34de451bd19fa8e220520a2
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-07-12 03:24:05 +0600

    Working on nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import Pricing from './components/Pricing/Pricing';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is big big header</h1>
+      <NavBar></NavBar>
+      <h1 className="text-6xl font-bold underline">This is big big header</h1>
       <p>This is only a paragraph</p>
+      <Pricing></Pricing>
     </div>
   );
 }
